742b6cbee16515ac6789816712de79a0c52c4e73
[ArseneLupinhb] <[1261175010@qq.com]>
2019-07-24 22:00:33 +0800

    Initial commit

---
+++ tensorflow/product_ML/estimator_linear_model.py
@@ -0,0 +1,20 @@
+# not complete 2019.7.20
+import tensorflow as tf
+import tensorflow.feature_column as fc
+
+import os
+import sys
+
+import matplotlib.pyplot as plt
+from IPython.display import clear_output
+
+
+tf.enable_eager_execution()
+models_path = os.path.join(os.getcwd(), 'models')
+
+sys.path.append(models_path)
+
+from official.wide_deep import census_dataset
+from official.wide_deep import census_main
+
+census_dataset.download("/tmp/census_data/")
